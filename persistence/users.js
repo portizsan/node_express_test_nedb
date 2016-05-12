0761ee1161d4f6056573d02ebdfa6360c61886e8
Pablo <portizsan@gmail.com>
2016-05-12 10:15:09 +0000

    deleted camo

--- persistence/users.js
+++ persistence/users.js
@@ -1,25 +1,30 @@
-var connect = require('camo').connect;
-var userSchema = require('../schema/user');
+var Datastore = require('nedb'),
+    db = new Datastore({
+        filename: '../data/users.db',
+        autoload: true
+    });
 
-var database;
-var uri = 'nedb://../data/users';
-connect(uri).then(function(db) {
-    database = db;
-});
 
 module.exports = {
     getAll: function(callback) {
-        userSchema.find({}, {
-            populate: true
-        }).then(function(users) {
-            callback(null, users);
+        db.find({}, function(err, users) {
+            if (!err) {
+                callback(null, users);
+            }
+            else {
+                callback(err)
+            }
         });
     },
     create: function(user, callback) {
-        var newUser = userSchema.create(user);
-        newUser.save().then(function(u) {
-            console.log(u._id);
-            callback(u);
+        db.insert(user, function(err, u) {
+            if (!err) {
+                console.log(u._id);
+                callback(u);
+            }
+            else {
+                callback(err)
+            }
         });
     }
 
